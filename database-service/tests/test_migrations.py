99114cbe9755f87086cd22df7f2c78167834f971
Laurent Michel <laurent.michel@ruag.com>
2019-10-25 18:17:44 +0200

    * install sh module * run migration command and verify that it returns 0

--- database-service/tests/test_migrations.py
+++ database-service/tests/test_migrations.py
@@ -1,2 +1,13 @@
+import sh
+
 def test_migrations_can_be_applied_without_errors():
-    assert True == False
+    # 1. Check that the migration status is fine
+    # assert we see the migrations folder from here
+
+    # 2. Apply migration
+    # TODO: put the endpoint in a fixture in conftest.py
+    cmd = sh.Command('hasura')
+    result = cmd('--skip-update-check', 'migrate', 'apply', '--endpoint', 'http://localhost:8080')
+
+    # 3. Verify that the migration performed without errors
+    assert 0 == result.exit_code
