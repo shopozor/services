242516ba26eeb7a9a43169b8a3b3a526435246c6
Laurent Michel <laurent.michel@ruag.com>
2019-10-26 15:53:31 +0200

    try to start, run, and stop docker compose

--- Test.groovy
+++ Test.groovy
@@ -5,11 +5,10 @@ pipeline {
   }
   stages {
     stage('GraphQL engine tests') {
-    //   steps {
-    //     sh ". $VENV/bin/activate && pytest -ra --junitxml=$REPORTS_FOLDER/shopozor-unit-tests.xml"
-    //   }
       steps {
         step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose-tests.yml', option: [$class: 'StartAllServices'], useCustomDockerComposeFile: true])
+        step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose-tests.yml', option: [$class: 'ExecuteCommandInsideContainer', command: 'pytest --hasura-endpoint http://graphql-engine:8080 -ra --junitxml=test-report.xml', index: 1, privilegedMode: false, service: 'hasura-service-tests', workDir: ''], useCustomDockerComposeFile: true])
+        step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose-tests.yml', option: [$class: 'StopAllServices'], useCustomDockerComposeFile: true])
       }
     }
   }
