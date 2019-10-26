e5ed899c364bf60d12fb4df054b77f8b5a9d174f
Laurent Michel <laurent.michel@ruag.com>
2019-10-26 11:27:54 +0200

    working, incomplete test

--- database-service/tests/test_migrations.py
+++ database-service/tests/test_migrations.py
@@ -4,10 +4,8 @@ def test_migrations_can_be_applied_without_errors():
     # 1. Check that the migration status is fine
     # assert we see the migrations folder from here
 
-    # 2. Apply migration
     # TODO: put the endpoint in a fixture in conftest.py
     cmd = sh.Command('hasura')
-    result = cmd('--skip-update-check', 'migrate', 'apply', '--endpoint', 'http://localhost:8080')
+    result = cmd('migrate', 'apply', '--endpoint', 'http://graphql-engine:8080', '--skip-update-check')
 
-    # 3. Verify that the migration performed without errors
     assert 0 == result.exit_code
