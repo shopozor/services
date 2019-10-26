c0c77bfb6b46148a44f31b01f12efdf0afb6f859
Laurent Michel <laurent.michel@ruag.com>
2019-10-26 13:52:48 +0200

    add missing test step

--- database-service/tests/test_migrations.py
+++ database-service/tests/test_migrations.py
@@ -1,10 +1,13 @@
 import sh
 
 def test_migrations_can_be_applied_without_errors(hasura_endpoint):
-    # 1. Check that the migration status is fine
-    # assert we see the migrations folder from here
+    # Given I am in the right location
+    assert 0 == sh.ls('migrations').exit_code 
+    assert 0 == sh.ls('config.yaml').exit_code
 
+    # When I apply the migrations
     cmd = sh.Command('hasura')
     result = cmd('migrate', 'apply', '--endpoint', hasura_endpoint, '--skip-update-check')
 
+    # Then I get no errors
     assert 0 == result.exit_code
