a1478acb52571cb2425f4084b6d8a6e6ef4e4db3
Laurent Michel <laurent.michel@ruag.com>
2019-10-26 16:01:02 +0200

    try to abort on container exit

--- Test.groovy
+++ Test.groovy
@@ -6,9 +6,10 @@ pipeline {
   stages {
     stage('GraphQL engine tests') {
       steps {
-        step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose-tests.yml', option: [$class: 'StartAllServices'], useCustomDockerComposeFile: true])
-        step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose-tests.yml', option: [$class: 'ExecuteCommandInsideContainer', command: 'pytest --hasura-endpoint http://graphql-engine:8080 -ra --junitxml=test-report.xml', index: 1, privilegedMode: false, service: 'hasura-service-tests', workDir: ''], useCustomDockerComposeFile: true])
-        step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose-tests.yml', option: [$class: 'StopAllServices'], useCustomDockerComposeFile: true])
+        // step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose-tests.yml', option: [$class: 'StartAllServices'], useCustomDockerComposeFile: true])
+        // step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose-tests.yml', option: [$class: 'ExecuteCommandInsideContainer', command: 'pytest --hasura-endpoint http://graphql-engine:8080 -ra --junitxml=test-report.xml', index: 1, privilegedMode: false, service: 'hasura-service-tests', workDir: ''], useCustomDockerComposeFile: true])
+        // step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose-tests.yml', option: [$class: 'StopAllServices'], useCustomDockerComposeFile: true])
+        sh "docker-compose -f /var/jenkins_home/workspace/docker-compose-test/docker-compose-tests.yml up --abort-on-container-exit"
       }
     }
   }
