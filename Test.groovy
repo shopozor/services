1a01f73256406c829a4117dda2ada2fb701cb85b
Laurent Michel <laurent.michel@ruag.com>
2019-10-27 14:03:42 +0100

    clean up comments

--- Test.groovy
+++ Test.groovy
@@ -4,8 +4,6 @@ pipeline {
     REPORTS_FOLDER = 'junit-reports'
   }
   stages {
-    // sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    // sudo chmod +x /usr/local/bin/docker-compose
     stage('GraphQL engine tests') {
       steps {
         sh "docker-compose -f /var/jenkins_home/workspace/docker-compose-test/docker-compose-tests.yaml up --abort-on-container-exit"
