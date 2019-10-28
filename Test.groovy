10550fcb109f4283b1d254dae553120b20610315
Laurent Michel <laurent.michel@ruag.com>
2019-10-28 11:48:46 +0100

    explicitly specify the services to be started

--- Test.groovy
+++ Test.groovy
@@ -3,7 +3,7 @@ pipeline {
   stages {
     stage('GraphQL engine tests') {
       steps {
-        sh "docker-compose -f docker-compose-tests.yaml up --abort-on-container-exit"
+        sh "docker-compose -f docker-compose-tests.yaml up postgres graphql-engine hasura-service-tests --abort-on-container-exit"
       }
     }
   }
