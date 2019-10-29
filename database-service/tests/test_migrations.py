4123016e8d7b989ca299725d1ff2ccec447ffc3c
Laurent Michel <laurent.michel@ruag.com>
2019-10-29 06:47:26 +0100

    fix checks on file / folder exists

--- database-service/tests/test_migrations.py
+++ database-service/tests/test_migrations.py
@@ -1,13 +1,16 @@
+import os
 import sh
+
 
 def test_migrations_can_be_applied_without_errors(hasura_endpoint):
     # Given I am in the right location
-    assert 0 == sh.ls('migrations').exit_code 
-    assert 0 == sh.ls('config.yaml').exit_code
+    assert os.path.exists('migrations')
+    assert os.path.isfile('config.yaml')
 
     # When I apply the migrations
     cmd = sh.Command('hasura')
-    result = cmd('migrate', 'apply', '--endpoint', hasura_endpoint, '--skip-update-check')
+    result = cmd('migrate', 'apply', '--endpoint',
+                 hasura_endpoint, '--skip-update-check')
 
     # Then I get no errors
     assert 0 == result.exit_code
