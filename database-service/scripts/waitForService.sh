7eb602c5698c5eea5e47536d5dc683c9993a97a0
Laurent Michel <laurent.michel@ruag.com>
2019-11-04 08:47:34 +0100

    rework waitForService script

--- database-service/scripts/waitForService.sh
+++ database-service/scripts/waitForService.sh
@@ -5,14 +5,14 @@ HOST=${1:-graphql-engine}
 PORT=${2:-8080}
 
 wait_for_port() {
-    echo "waiting $HASURA_GRAPHQL_MIGRATIONS_SERVER_TIMEOUT for $PORT to be ready"
+    echo "waiting $HASURA_GRAPHQL_MIGRATIONS_SERVER_TIMEOUT for $PORT to be ready on host $HOST"
     for i in `seq 1 $HASURA_GRAPHQL_MIGRATIONS_SERVER_TIMEOUT`;
     do
         echo "waiting for graphql engine"
-        nc -z $HOST $PORT
-        [ "$?" == "0" ] && return || sleep 1
+        nc -z $HOST $PORT > /dev/null 2>&1 && echo "port $PORT is ready on host $HOST" && return
+        sleep 1
     done
-    echo "failed waiting for $PORT" && exit 1
+    echo "failed waiting for $PORT on host $HOST" && exit 1
 }
 
 wait_for_port
