9b7ae745763a2c3faf7ebcd5b6c4931c7a3c2fa1
Laurent Michel <laurent.michel@ruag.com>
2019-11-04 17:20:52 +0100

    Refactoring
    * give --root argument to pytest pointing to /app
    * adapt unit test

--- database-service/tests/conftest.py
+++ database-service/tests/conftest.py
@@ -6,8 +6,15 @@ def pytest_addoption(parser):
     parser.addoption(
         "--hasura-endpoint", action="store", default="http://localhost:8080", help="Hasura endpoint"
     )
+    parser.addoption(
+        "--root", action="store", default="/app", help="Database service root folder"
+    )
 
 
 @pytest.fixture
 def hasura_endpoint(request):
     return request.config.getoption("--hasura-endpoint")
+
+@pytest.fixture 
+def app_root_folder(request):
+    return request.config.getoption("--root")
--- database-service/tests/test_migrations.py
+++ database-service/tests/test_migrations.py
@@ -2,15 +2,13 @@ import os
 import sh
 
 
-def test_migrations_can_be_applied_without_errors(hasura_endpoint):
-    # Given I am in the right location
-    assert os.path.exists('migrations')
-    assert os.path.isfile('config.yaml')
-
+def test_shopozor_structural_migrations_can_be_applied_without_errors(hasura_endpoint, app_root_folder):
     # When I apply the migrations
     cmd = sh.Command('hasura')
     result = cmd('migrate', 'apply', '--endpoint',
-                 hasura_endpoint, '--skip-update-check')
+                 hasura_endpoint, '--project', app_root_folder, '--skip-update-check')
 
     # Then I get no errors
     assert 0 == result.exit_code
+
+    # 2. --project=/app/fixtures (after they have been generated)
