8a1defd5d2e05ceac7a1325f1ab8f358e920e6b8
Laurent Michel <laurent.michel@ruag.com>
2019-11-07 13:13:52 +0100

    simplify the tests as much as possible

--- database-service/tests/conftest.py
+++ database-service/tests/conftest.py
@@ -1,9 +1,6 @@
 import os
-import psycopg2
 import pytest
-from urllib.parse import urljoin
 
-from utils.database import DatabaseHandler
 from utils.migrations import HasuraClient, FixturesGenerator
 
 
@@ -27,26 +24,10 @@ def app_root_folder(request):
 
 
 @pytest.fixture
-def postgres_connection():
-    conn = psycopg2.connect(
-        host='postgres', database='postgres', user='postgres')
-    yield conn
-    conn.close()
-
-
-@pytest.fixture
-def enum_table_names():
-    return sorted(('vat_types', 'pricing_modes', 'product_states', 'productvariant_states', 'roles'))
-
-
-@pytest.fixture
-def db_handler(postgres_connection):
-    return DatabaseHandler(postgres_connection)
-
-
-@pytest.fixture
-def hasura_client(hasura_endpoint, db_handler):
-    return HasuraClient(hasura_endpoint)
+def hasura_client(hasura_endpoint, app_root_folder):
+    client = HasuraClient(hasura_endpoint)
+    yield client
+    client.rollback_migrations(app_root_folder)
 
 
 @pytest.fixture
@@ -60,7 +41,8 @@ def fixtures_generator(app_root_folder, fixtures_project_folder):
 
 
 @pytest.fixture
-def small_fixtures(fixtures_generator):
+def small_fixtures(fixtures_generator, hasura_client):
     fixtures_generator.generate('small')
     yield fixtures_generator.project_folder()
+    hasura_client.rollback_migrations(fixtures_generator.project_folder())
     fixtures_generator.cleanup()
--- database-service/tests/test_migrations.py
+++ database-service/tests/test_migrations.py
@@ -1,36 +1,22 @@
-def test_shopozor_structural_migrations_can_be_rolled_back(hasura_client, app_root_folder, db_handler):
-    # Given I've applied structural migrations
+def test_shopozor_structural_migrations_can_be_applied(hasura_client, app_root_folder):
+    # Given I have structural migrations
     project_folder = app_root_folder
+
+    # When I apply them
     result = hasura_client.apply_migrations(project_folder)
+
+    # Then I get no error
     assert 0 == result.exit_code
 
-    # When I revert the migrations
-    result = hasura_client.rollback_migrations(project_folder)
 
-    # Then I get no errors
-    assert 0 == result.exit_code
-    # And the database is empty
-    assert db_handler.is_database_empty()
-
-
-def test_fixtures_migrations_can_be_rolled_back(hasura_client, app_root_folder, small_fixtures, db_handler, enum_table_names):
-    # Given I've applied fixtures migrations
+def test_fixtures_migrations_can_be_applied(hasura_client, app_root_folder, small_fixtures):
+    # Given I've applied structural migrations
     structural_project_folder = app_root_folder
-    hasura_client.apply_migrations(structural_project_folder)
-    hasura_client.apply_migrations(small_fixtures)
-
-    # When I revert the fixtures data
-    result = hasura_client.rollback_migrations(small_fixtures)
-
-    # Then I get no errors
+    result = hasura_client.apply_migrations(structural_project_folder)
     assert 0 == result.exit_code
-    # And only the enum tables are not empty
-    assert enum_table_names == db_handler.get_non_empty_tables()
 
-    # When I revert the structural migrations
-    result = hasura_client.rollback_migrations(structural_project_folder)
+    # When I apply the fixtures migrations
+    result = hasura_client.apply_migrations(small_fixtures)
 
-    # Then I get no errors
+    # Then I get no error
     assert 0 == result.exit_code
-    # And the database is empty
-    assert db_handler.is_database_empty()
