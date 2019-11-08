dd78acafbabb9463026bef5e0d99fcc90f307f69
Laurent Michel <laurent.michel@softozor.ch>
2019-11-08 17:43:24 +0100

    check migrations status during the tests

--- database-service/tests/utils/migrations.py
+++ database-service/tests/utils/migrations.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sh
 
 
@@ -7,12 +8,26 @@ class HasuraClient:
         self.__cli = sh.Command('hasura')
         self.__endpoint = hasura_endpoint
 
-    # TODO: check that the migrations are applied!
+    def __get_not_present_migrations_count(self, project_folder):
+        migrations_folder = os.path.join(project_folder, 'migrations')
+        relevant_timestamps = [item.split('_')[0]
+                               for item in os.listdir(migrations_folder)]
+        statuses = self.__cli('migrate', 'status', '--endpoint', self.__endpoint,
+                              '--project', project_folder, '--skip-update-check')
+        result = 0
+        for timestamp in relevant_timestamps:
+            status = re.findall(f'^{timestamp}.*$',
+                                str(statuses), re.MULTILINE)[0]
+            result += status.count('Not Present')
+        return result
 
     def apply_migrations(self, project_folder):
         result = self.__cli('migrate', 'apply', '--endpoint', self.__endpoint,
                             '--project', project_folder, '--skip-update-check')
-        return result.exit_code
+        if result.exit_code == 0:
+            migrations_folder = os.path.join(project_folder, 'migrations')
+            return self.__get_not_present_migrations_count(project_folder) == 0
+        return False
 
     def __get_number_of_migrations_in_folder(self, folder):
         return int(len(os.listdir(folder)) / 2)
@@ -23,4 +38,6 @@ class HasuraClient:
             migrations_folder)
         result = self.__cli('migrate', 'apply', '--down', nb_migrations, '--endpoint',
                             self.__endpoint, '--project', project_folder, '--skip-update-check')
-        return result.exit_code
+        if result.exit_code == 0:
+            return self.__get_not_present_migrations_count(project_folder) == nb_migrations
+        return False
