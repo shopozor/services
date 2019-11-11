a9288d9b563b716f87738da8d60b55dececf1e32
Laurent Michel <laurent.michel@softozor.ch>
2019-11-11 07:43:51 +0100

    hopefully fix waiting for hasura

--- database-service/scripts/waitForService.sh
+++ database-service/scripts/waitForService.sh
@@ -9,8 +9,8 @@ wait_for_port() {
     for i in `seq 1 $HASURA_GRAPHQL_MIGRATIONS_SERVER_TIMEOUT`;
     do
         echo "waiting for graphql engine"
-        nc -z $HOST $PORT
-        [ "$?" == "0" ] && return || sleep 1
+        version=$(curl -s http://$HOST:$PORT/v1/version | jq ".version")
+        [ "$version" == "" ] && sleep 1 || return
     done
     echo "failed waiting for $PORT on host $HOST" && exit 1
 }
