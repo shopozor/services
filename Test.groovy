e3a13235accfcc53af2557b61da0802d2b1c2684
Laurent Michel <laurent.michel@softozor.ch>
2019-11-13 12:25:29 +0100

    fix duplicated database fixtures generation stage

--- Test.groovy
+++ Test.groovy
@@ -16,11 +16,6 @@ pipeline {
         }
       }
     }
-    stage('Generate the database fixtures') {
-      steps {
-        sh "docker-compose -f docker-compose-tests.yaml up fixtures-service"
-      }
-    }
     stage('Start GraphQL engine') {
       steps {
         sh "docker-compose -f docker-compose-tests.yaml up -d postgres graphql-engine"
