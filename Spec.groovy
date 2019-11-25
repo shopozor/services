c7f90dfd532dcf1eed83e77cf7bf927e20eaef1e
Laurent Michel <laurent.michel@ruag.com>
2019-11-25 22:32:14 +0100

    fix spec pipeline

--- Spec.groovy
+++ Spec.groovy
@@ -6,7 +6,12 @@ pipeline {
         SOFTOZOR_CREDENTIALS = credentials('softozor-credentials')
       }
       steps {
-        helpers.generateSpecification("$WORKSPACE/ui/cypress/e2e", $REPO, GIT_COMMIT)
+        script {
+          featureDir = "$WORKSPACE/ui/cypress/e2e"
+          sh "mono /opt/pickles/Pickles.exe --feature-directory=$featureDir --output-directory=specification --system-under-test-name=shopozor --system-under-test-version=$GIT_COMMIT --language=fr --documentation-format=dhtml --exp --et 'in-preparation'"
+          sh "sshpass -p $SOFTOZOR_CREDENTIALS_PSW ssh -o StrictHostKeyChecking=no $SOFTOZOR_CREDENTIALS_USR@softozor.ch 'rm -Rf ~/www/www.softozor.ch/shopozor/*'"
+          sh "sshpass -p $SOFTOZOR_CREDENTIALS_PSW scp -o StrictHostKeyChecking=no -r specification/* $SOFTOZOR_CREDENTIALS_USR@softozor.ch:~/www/www.softozor.ch/shopozor/"
+        }
       }
     }
   }
