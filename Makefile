c304e4de9c406488a182ba2394095ab9d505e29c
Laurent Michel <laurent.michel@ruag.com>
2019-11-27 21:22:53 +0100

    revert last commit

--- Makefile
+++ Makefile
@@ -36,8 +36,8 @@ db.migrate.status:
 
 fixtures.generate:
 	@echo "Generating fixtures ..."
-	# @chmod u+x ./fixtures-generator/entrypoint.sh
-	$(shell USER_ID=1000 chmod u+x ./fixtures-generator/entrypoint.sh && docker-compose -f docker-compose.yaml -f docker-compose-tests.yaml -f docker-compose-tests-dev.yaml up fixtures-service)
+	@chmod u+x ./fixtures-generator/entrypoint.sh
+	@docker-compose -f docker-compose.yaml -f docker-compose-tests.yaml -f docker-compose-tests-dev.yaml up fixtures-service
 	@docker-compose -f docker-compose.yaml -f docker-compose-tests.yaml rm -f fixtures-service
 
 fixtures.up:
--- Test.groovy
+++ Test.groovy
@@ -15,9 +15,8 @@ pipeline {
           sh "make fixtures.clean"
           sh "mkdir fixtures && mkdir -p graphql/responses"
           // without that USER_ID variable, it is not possible to delete the generated fixtures folder anymore
-          // sh "chmod u+x ./fixtures-generator/entrypoint.sh"
-          // sh "USER_ID=`id -u` docker-compose -f docker-compose.yaml -f docker-compose-tests.yaml up fixtures-service"
-          sh "make fixtures.generate"
+          sh "chmod u+x ./fixtures-generator/entrypoint.sh"
+          sh "USER_ID=`id -u` docker-compose -f docker-compose.yaml -f docker-compose-tests.yaml up fixtures-service"
         }
       }
     }
