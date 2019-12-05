a0197ac0b13d25e44836f4f0d6a7a3c71db5a765
Laurent Michel <laurent.michel@ruag.com>
2019-12-05 12:12:08 +0100

    trigger spec and docker image publication upon failure to see if it works

--- Test.groovy
+++ Test.groovy
@@ -72,7 +72,7 @@ pipeline {
       sh "make down"
       junit "**/test-reports/*.xml"
     }
-    success {
+    failure {
       build job: 'specification', parameters: [
         string(name: 'BRANCH', value: GIT_BRANCH.split('/')[1])
       ]
