f0ec4e04faf3cd13744a469e91d5b525f1520729
Laurent Michel <laurent.michel@ruag.com>
2019-12-16 18:02:47 +0100

    add todo on map specification

--- frontend/consumer-ui/components/Map.stories.js
+++ frontend/consumer-ui/components/Map.stories.js
@@ -12,3 +12,6 @@ storiesOf('Map', module)
       template: '<shops-map/>'
     }
   })
+  // TODO: need to test what happens in case there is no data
+  // TODO: by default --> max zoom out to see the whole switzerland
+  // TODO: later: if a consumer has an account, center on her location
