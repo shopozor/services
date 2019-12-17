363c58978f0eefef0fcfb8f4c9b17bb27e336b33
Laurent Michel <laurent.michel@ruag.com>
2019-12-17 20:25:14 +0100

    install storybook viewport addon

--- frontend/consumer-ui/.storybook/config.js
+++ frontend/consumer-ui/.storybook/config.js
@@ -1,5 +1,6 @@
-import apolloStorybookDecorator from "apollo-storybook-vue"
-import { configure, addDecorator } from '@storybook/vue'
+import apolloStorybookDecorator from 'apollo-storybook-vue'
+import { configure, addDecorator, addParameters } from '@storybook/vue'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import Vue from 'vue'
 
 import typeDefs from './schema/typeDefinitions'
@@ -7,6 +8,15 @@ import mocks from './schema/mocks'
 
 import '../plugins/errorHandling'
 import '../plugins/leaflet'
+
+// cf. https://github.com/storybookjs/storybook/tree/master/addons/viewport
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+    // TODO: this does not seem to be working!
+    defaultViewport: 'mobile'
+  },
+})
 
 addDecorator(
   apolloStorybookDecorator({
