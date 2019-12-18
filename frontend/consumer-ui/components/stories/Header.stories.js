eefd78d43236ea02251b8fc04406baef03cba2cd
Laurent Michel <laurent.michel@ruag.com>
2019-12-18 10:46:56 +0100

    header rework

--- frontend/consumer-ui/components/stories/Header.stories.js
+++ frontend/consumer-ui/components/stories/Header.stories.js
@@ -1,15 +1,15 @@
 import { storiesOf } from '@storybook/vue'
 
-import Header from '../Header'
+import ShopozorHeader from '../Header'
 
 const components = {
-  'my-header': Header
+  ShopozorHeader
 }
 
 storiesOf('Header', module)
   .add('Header', () => {
     return {
       components,
-      template: '<my-header />'
+      template: '<shopozor-header />'
     }
   })
