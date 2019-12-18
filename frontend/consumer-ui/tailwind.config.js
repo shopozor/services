b597d64c8482ec8c423f655838f29bf16bc4c7e8
Laurent Michel <laurent.michel@ruag.com>
2019-12-18 13:02:16 +0100

    define our own stylings as important

--- frontend/consumer-ui/tailwind.config.js
+++ frontend/consumer-ui/tailwind.config.js
@@ -5,6 +5,7 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
+  important: true,
   theme: {
     screens: {
       xs: '320px',
