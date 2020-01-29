67f4b630874608dcf39e5a6bb62735dcb0d9200e
Laurent Michel <laurent.michel@ruag.com>
2020-01-29 09:40:08 +0100

    fix cypress eslint / jest configuration

--- frontend/consumer-ui/cypress/e2e/IncognitoVisitsShop/main.spec.js
+++ frontend/consumer-ui/cypress/e2e/IncognitoVisitsShop/main.spec.js
@@ -2,5 +2,5 @@ import { Given } from 'cypress-cucumber-preprocessor/steps'
 
 Given('un utilisateur non identifié', function () {
   // TODO: put this id string 'token' in a dedicated global variable somewhere
-  // cy.getCookie('token').then(token => expect(token).not.toBe(null))
+  cy.getCookie('token').then(token => expect(token).to.be.null)
 })
