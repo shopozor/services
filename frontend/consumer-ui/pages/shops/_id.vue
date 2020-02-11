8ad10003304029bf695bb781342acd1ceb035bf6
Laurent Michel <laurent.michel@ruag.com>
2020-02-11 14:45:19 +0100

    get rid of initial article page

---
+++ frontend/consumer-ui/pages/shops/_id.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ shop.name }}</h1>
+    C'est la Budzonnerie numéro {{ shop.id }}
+  </div>
+</template>
+
+<script>
+import shop from '~graphql/shop'
+
+export default {
+  apollo: {
+    shop: {
+      query: shop,
+      // TODO: what is this for exactly?
+      prefetch: ({ route }) => ({ id: route.params.id }),
+      variables () {
+        return { shopId: this.$route.params.id }
+      }
+    }
+  },
+  head () {
+    return {
+      title: shop ? shop.name : 'Loading'
+    }
+  }
+}
+</script>
