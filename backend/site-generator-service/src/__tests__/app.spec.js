5d17d68c15a497f77ba97e3e5a1e51ef9068ae31
Laurent Michel <laurent.michel@ruag.com>
2020-02-13 10:06:37 +0100

    * add commented out test * add TODO comment on dev-test

--- backend/site-generator-service/src/__tests__/app.spec.js
+++ backend/site-generator-service/src/__tests__/app.spec.js
@@ -1,9 +1,17 @@
 const request = require('supertest')
 const app = require('../app')
 
-describe('Test the root path', () => {
-  it('should return 200 upon browsing /', async () => {
+describe('Test root path', () => {
+  it('should return 404 upon browsing /', async () => {
     const response = await request(app).get('/')
-    expect(response.statusCode).toBe(200)
+    expect(response.statusCode).toBe(404)
   })
 })
+
+// TODO: to test this, we need to mock the generator, otherwise jest will fail and we also generate a website, which is not what we want
+/* describe('Test generate path', () => {
+  it('should return 200 upon browsing /generate', async () => {
+    const response = await request(app).get('/generate')
+    expect(response.statusCode).toBe(200)
+  })
+}) */
