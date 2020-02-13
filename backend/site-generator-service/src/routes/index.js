e1ed99f2129e6434bb0bb4d1c2f8e32969267c60
Laurent Michel <laurent.michel@ruag.com>
2020-02-13 10:05:57 +0100

    add commented out test

--- backend/site-generator-service/src/routes/index.js
+++ backend/site-generator-service/src/routes/index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.status(200)
-    .send('Hello world!')
+  res.status(404)
 })
 
 module.exports = router
