350f0cc5b75a773a8e1404447f802d3ad124e3ec
Laurent Michel <laurent.michel@ruag.com>
2020-02-28 12:59:21 +0100

    make backend service tests docker images available

--- backend/tests/entrypoint.sh
+++ backend/tests/entrypoint.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-HOST=${1:-api}
+HOST=${1:-graphql-engine}
 PORT=${2:-8080}
 
-wait-for-it $HOST:$PORT -t 60 -s -- pytest -ra --junitxml=test-reports/test-report.xml -vv --hasura-endpoint http://$HOST:$API
+wait-for-it $HOST:$PORT -t 60 -s -- pytest -ra --junitxml=test-reports/test-report.xml -vv --hasura-endpoint http://$HOST:$PORT
