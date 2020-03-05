6ef06d75fb5b4cf3aa7f969fd2ae6ec76badadab
Laurent Michel <laurent.michel@ruag.com>
2020-03-05 11:53:46 +0100

    * rework a bit production deployment * install letsencrypt * serve dashboard over subdomain * install cert-manager

--- scripts/install_k8s.sh
+++ scripts/install_k8s.sh
@@ -1,7 +1,21 @@
 #! /bin/bash
 
-# TODO: this is not necessary when we work with devspace!
-kubectl create namespace dev
-kubectl create namespace staging
-kubectl create namespace pre-production
-kubectl create namespace production
+if [ "$#" -ne "4" ] ; then
+  echo "Usage: $0 <base-url> <env-name> <k8s-dashboard-version> <cert-mgr-version>"
+  exit 1
+fi
+
+BASE_URL=$1
+ENV_NAME=$2
+K8S_DASHBOARD_VERSION=$3
+CERT_MANAGER_VERSION=$4
+
+# Transform dashboard ingress from /kubernetes-dashboard path to dashboard subdomain
+dashboard_ingress_yaml="dashboard-ingress.yaml"
+kubectl delete ingress kubernetes-dashboard
+wget -q ${BASE_URL}/manifests/${dashboard_ingress_yaml} -O ${dashboard_ingress_yaml}
+sed -i "s/ENV/${ENV_NAME}.hidora.com/g" ${dashboard_ingress_yaml}
+
+# Activate TLS
+kubectl create namespace cert-manager
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cert-manager.yaml
