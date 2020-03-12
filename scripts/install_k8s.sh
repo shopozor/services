7c435d99fde2a0ea63b11ab8fcfa55d52de9acdf
Laurent Michel <laurent.michel@ruag.com>
2020-03-12 18:10:09 +0100

    * use production certification manager * ask user email upon jps manifest installation * add yaml manifests to install cluster issuers * let the install_k8s script install the cluster issuers

--- scripts/install_k8s.sh
+++ scripts/install_k8s.sh
@@ -1,12 +1,22 @@
 #! /bin/bash
 
-if [ "$#" -ne "1" ] ; then
-  echo "Usage: $0 <cert-mgr-version>"
+if [ "$#" -ne "3" ] ; then
+  echo "Usage: $0 <base-url> <cert-mgr-version> <cert-mgr-email>"
   exit 1
 fi
 
-CERT_MANAGER_VERSION=$1
+BASE_URL=$1
+CERT_MANAGER_VERSION=$2
+CERT_MANAGER_EMAIL=$3
 
 # Install cert-manager
 kubectl create namespace cert-manager
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cert-manager.yaml
+
+# Install certification issuers
+wget ${BASE_URL}/manifests/staging_issuer.yaml -O staging_issuer.yaml
+wget ${BASE_URL}/manifests/prod_issuer.yaml -O prod_issuer.yaml
+sed -i "s/EMAIL_ADDRESS/${CERT_MANAGER_EMAIL}/g" staging_issuer.yaml
+kubectl create -f staging_issuer.yaml
+sed -i "s/EMAIL_ADDRESS/${CERT_MANAGER_EMAIL}/g" prod_issuer.yaml
+kubectl create -f prod_issuer.yaml
