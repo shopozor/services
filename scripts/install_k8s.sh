2f537996eb5f10a77bd4b8906b4e7e239e22de6c
Laurent Michel <laurent.michel@ruag.com>
2020-03-17 15:35:57 +0100

    take over ssl configuration we did for the jenkins environment

--- scripts/install_k8s.sh
+++ scripts/install_k8s.sh
@@ -1,25 +1,3 @@
 #! /bin/bash
 
-if [ "$#" -ne "3" ] ; then
-  echo "Usage: $0 <base-url> <cert-mgr-version> <cert-mgr-email>"
-  exit 1
-fi
-
-BASE_URL=$1
-CERT_MANAGER_VERSION=$2
-CERT_MANAGER_EMAIL=$3
-
-installIssuer() {
-  local name=$1
-  wget ${BASE_URL}/manifests/${name}.yaml -O ${name}.yaml
-  sed -i "s/EMAIL_ADDRESS/${CERT_MANAGER_EMAIL}/g" ${name}.yaml
-  kubectl create -f ${name}.yaml
-}
-
-# Install cert-manager
-kubectl create namespace cert-manager
-kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cert-manager.yaml
-
-# Install certification issuers
-installIssuer staging_issuer
-installIssuer prod_issuer
+echo "Doing nothing!"
