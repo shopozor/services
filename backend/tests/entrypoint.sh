4767412d20a534cb498f7ba6d154afeaf7f0b071
Laurent Michel <laurent.michel@ruag.com>
2020-03-21 15:54:53 +0100

    fix service tests

--- backend/tests/entrypoint.sh
+++ backend/tests/entrypoint.sh
@@ -2,5 +2,7 @@
 
 HOST=${1:-graphql-engine}
 PORT=${2:-8080}
+ROOT_DIR=${3:-/app}
 
-wait-for-it $HOST:$PORT -t 60 -s -- ./run_tests.sh http://$HOST:$PORT
+cd ${ROOT_DIR}/backend/tests
+wait-for-it $HOST:$PORT -t 60 -s -- ${ROOT_DIR}/backend/tests/run_tests.sh http://$HOST:$PORT ${ROOT_DIR}
--- backend/tests/run_tests.sh
+++ backend/tests/run_tests.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
-if [ "$#" -ne "1" ] ; then
-  echo "Usage: $0 <endpoint>"
-  echo "Example: $0 http://graphql-engine:8080"
+if [ "$#" -ne "2" ] ; then
+  echo "Usage: $0 <endpoint> <root-dir>"
+  echo "Example: $0 http://graphql-engine:8080 /app"
   exit 1
 fi
 
 ENDPOINT=$1
+ROOT_DIR=$2
 
-hasura migrate apply --endpoint $ENDPOINT --project ./fixtures/database --up all --skip-update-check
-pytest -ra --junitxml=test-reports/test-report.xml -vv --hasura-endpoint $ENDPOINT
-hasura migrate apply --endpoint $ENDPOINT --project ./fixtures/database --down $(ls ./fixtures/database/migrations/*.up.sql | wc -l) --skip-update-check
+hasura migrate apply --endpoint="$ENDPOINT" --project=${ROOT_DIR}/shared/fixtures/database --up all --skip-update-check
+pytest -p no:cacheprovider -ra --junitxml=${ROOT_DIR}/backend/tests/test-reports/test-report.xml -vv --hasura-endpoint="$ENDPOINT" --fixtures-folder="${ROOT_DIR}/shared/fixtures" --graphql-responses-folder="${ROOT_DIR}/shared/fixtures/graphql" --graphql-calls-folder="${ROOT_DIR}/shared/graphql" ${ROOT_DIR}/backend/tests
+hasura migrate apply --endpoint="$ENDPOINT" --project=${ROOT_DIR}/shared/fixtures/database --down $(ls ${ROOT_DIR}/shared/fixtures/database/migrations/*.up.sql | wc -l) --skip-update-check
