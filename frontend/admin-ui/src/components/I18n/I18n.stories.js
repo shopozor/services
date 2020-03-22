b39181638ae03685db5520e83735958b2a70fc2f
Kilian Thomas <kili236@gmail.com>
2020-03-22 12:30:18 +0100

    Fix & Update snapshots

--- frontend/admin-ui/src/components/I18n/I18n.stories.js
+++ frontend/admin-ui/src/components/I18n/I18n.stories.js
@@ -1,6 +1,6 @@
 import { storiesOf } from '@storybook/vue'
 import VueI18n from 'vue-i18n'
-import LanguageSelect from './LanguageSelect'
+import LanguageSelect from './LanguageSelect.vue'
 
 const components = {
   LanguageSelect
@@ -9,7 +9,7 @@ const components = {
 storiesOf('I18n/LanguageSelect', module)
   .add('Disable', () => ({
     components,
-    template: '<div style="background-color:lightblue"> <LanguageSelect /> </div>',
+    template: '<div style="background-color:lightblue"> <LanguageSelect id="languageSelect"/> </div>',
     i18n: new VueI18n({ locale: 'fr' })
   })
   )
--- frontend/admin-ui/src/components/I18n/LanguageSelect.vue
+++ frontend/admin-ui/src/components/I18n/LanguageSelect.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="locale-changer">
-    <q-select v-model="$i18n.locale" :options="['fr', 'en'] " dark />
+    <q-select v-model="$i18n.locale" :options="['fr', 'en'] " :for="id" dark />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'LanguageSelect'
+  name: 'LanguageSelect',
+  props: {
+    id: {
+      type: String,
+      default: () => null
+    }
+  }
 }
 </script>
