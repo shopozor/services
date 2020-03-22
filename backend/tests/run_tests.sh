5bef6e03f27cdeedc2d7dad30241586d98f21687
Laurent Michel <laurent.michel@ruag.com>
2020-03-22 15:53:50 +0100

    make a loop waiting for hasura migrate apply to be successful

--- backend/tests/run_tests.sh
+++ backend/tests/run_tests.sh
@@ -8,7 +8,12 @@ fi
 
 ENDPOINT=$1
 ROOT_DIR=$2
+TIMEOUT=60
 
-hasura migrate apply --endpoint="$ENDPOINT" --project=${ROOT_DIR}/shared/fixtures/database --up all --skip-update-check
+for i in `seq 1 $TIMEOUT`; do
+  echo "Trying $i / $TIMEOUT"
+  hasura migrate apply --endpoint="$ENDPOINT" --project=${ROOT_DIR}/shared/fixtures/database --up all --skip-update-check
+  [ "$?" == "0" ] && break || sleep 2
+done
 pytest -p no:cacheprovider -ra --junitxml=${ROOT_DIR}/backend/tests/test-reports/test-report.xml -vv --hasura-endpoint="$ENDPOINT" --fixtures-folder="${ROOT_DIR}/shared/fixtures" --graphql-responses-folder="${ROOT_DIR}/shared/fixtures/graphql" --graphql-calls-folder="${ROOT_DIR}/shared/graphql" ${ROOT_DIR}/backend/tests
 hasura migrate apply --endpoint="$ENDPOINT" --project=${ROOT_DIR}/shared/fixtures/database --down $(ls ${ROOT_DIR}/shared/fixtures/database/migrations/*.up.sql | wc -l) --skip-update-check
