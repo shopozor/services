1fe0558285378ec92ccc5c939a7c7b653b802ff2
Kilian Thomas <kili236@gmail.com>
2020-03-22 17:14:45 +0100

    port basic layout

---
+++ frontend/admin-ui/src/layouts/Layout.vue
@@ -0,0 +1,14 @@
+<template>
+  <parametricLayout
+    subtitle="store"
+  />
+</template>
+
+<script>
+import parametricLayout from './ParametricLayout'
+
+export default {
+  name: 'Layout',
+  components: { parametricLayout }
+}
+</script>
---
+++ frontend/admin-ui/src/layouts/ParametricLayout.vue
@@ -0,0 +1,72 @@
+<template>
+  <q-layout view="HHH Lpr lFf">
+    <q-header>
+      <q-toolbar
+        color="primary"
+      >
+        <q-btn
+          class="burger-menu"
+          flat
+          dense
+          round
+          aria-label="Menu"
+          :disable="disableNavigation"
+          @click="drawerOpen = !drawerOpen"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+
+        <q-toolbar-title>
+          {{ $t('shop') }} - {{ $t(subtitle) }}
+        </q-toolbar-title>
+        <language-select />
+      </q-toolbar>
+    </q-header>
+    <q-drawer
+      id="sideDrawer"
+      v-model="drawerOpen"
+      content-class="bg-sideDrawer"
+    >
+      <q-list>
+        <q-item-label header>
+          ENTER HEADER
+        </q-item-label>
+        <q-item clickable>
+          <q-item-section>
+            <q-item-label>ENTER LINK</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import LanguageSelect from '../components/I18n/LanguageSelect'
+
+export default {
+  name: 'Layout',
+  components: { LanguageSelect },
+  props: {
+    subtitle: {
+      type: String,
+      required: true
+    },
+    disableNavigation: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      drawerOpen: this.$q.platform.is.desktop && !this.disableNavigation
+    }
+  }
+}
+</script>
+
+<style>
+</style>
